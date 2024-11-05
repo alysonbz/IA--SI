b36f6d4355668aa78beffaeadbe52bccaf6061cf
alysonribeiro <alysonribeiro@mobitbrasil.com.br>
2024-11-05 12:05:35 -0300

    src

---
+++ Pre-processing/Unidade1/4_drop_nan.py
@@ -0,0 +1,6 @@
+from src.utils import load_volunteer_dataset
+
+volunteer = load_volunteer_dataset()
+
+#Elabore um código para motrar o shape e dados do dataset volunteer corrigido, sem NAN em suas colunas.
+#pronto para utilizar em um proxima etapa.
