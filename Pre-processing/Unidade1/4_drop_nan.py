0a17a98959afac6c69b2aece0f679a15cb7f749a
italocsr <tikoteko47@gmail.com>
2024-11-05 12:43:15 -0300

    tarefa de classe

--- Pre-processing/Unidade1/4_drop_nan.py
+++ Pre-processing/Unidade1/4_drop_nan.py
@@ -4,4 +4,13 @@ volunteer = load_volunteer_dataset()
 
 #Elabore um código para motrar o shape e dados do dataset volunteer corrigido, sem NAN em suas colunas.
 #pronto para utilizar em um proxima etapa.
+print('Dimensão do inicio: ', volunteer.shape)
 
+print(volunteer.isnull().sum())
+
+volunteer_coluna = volunteer.drop(['amsl', 'amsl_unit', 'is_priority', 'primary_loc'],axis=1)
+print(volunteer_coluna.isnull().sum())
+
+volunteer_line = volunteer_coluna.dropna()
+print(volunteer_line.isnull().sum())
+print(f'A dimensão final fica: ', volunteer_line.shape)
