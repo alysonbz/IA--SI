e6d49ca6b30fce3996ab165de1d6f509fc57efb2
Ruan Lima <ruanlimaspt@gmail.com>
2024-11-12 14:32:48 -0300

    atividades

--- Pre-processing/Unidade1/0_class_anotation.py
+++ Pre-processing/Unidade1/0_class_anotation.py
@@ -3,8 +3,18 @@ import pandas as pd
 
 volunteer = load_volunteer_dataset()
 hiking = load_hiking_dataset()
-wine  = load_wine_dataset()
+wine = load_wine_dataset()
 df1 = load_df1_unidade1()
 df2 = load_df2_unidade1()
 
-
+#print(hiking.head())
+#print(hiking.info())
+#print(wine.describe())
+#print(df1)
+#print(df1.dropna())
+#print(df1.drop([1, 2, 3]))
+#print(df1.drop("A", axis=1))
+print(df1.isna().sum())
+#print(df1.dropna(subset=["B"]))
+#print(df1.dropna(thresh=2))
+#print(volunteer.info())
--- Pre-processing/Unidade1/4_drop_nan.py
+++ Pre-processing/Unidade1/4_drop_nan.py
@@ -4,3 +4,5 @@ volunteer = load_volunteer_dataset()
 
 #Elabore um código para motrar o shape e dados do dataset volunteer corrigido, sem NAN em suas colunas.
 #pronto para utilizar em um proxima etapa.
+volunteer_nonnull = volunteer.drop('')
+volunteer_clean = volunteer.
