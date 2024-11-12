ee7a0b9b61437a460811c4133a81cad9c12f12ed
PauloJr000 <paulojr00@alu.ufc.br>
2024-11-12 11:00:30 -0300

    teste

--- Pre-processing/Unidade1/0_class_anotation.py
+++ Pre-processing/Unidade1/0_class_anotation.py
@@ -2,9 +2,25 @@ from src.utils import load_hiking_dataset , load_df2_unidade1,load_wine_dataset,
 import pandas as pd
 
 volunteer = load_volunteer_dataset()
+print(volunteer.info())
+
 hiking = load_hiking_dataset()
+print(hiking.info())
+
 wine  = load_wine_dataset()
+print(wine.describe())
+
 df1 = load_df1_unidade1()
+print(df1)
+print(df1.dropna())
+print(df1.dropna([1,2,3]))
+print()
+
 df2 = load_df2_unidade1()
+
+
+
+
+
 
 
--- Pre-processing/Unidade1/4_drop_nan.py
+++ Pre-processing/Unidade1/4_drop_nan.py
@@ -3,4 +3,14 @@ from src.utils import load_volunteer_dataset
 volunteer = load_volunteer_dataset()
 
 #Elabore um código para motrar o shape e dados do dataset volunteer corrigido, sem NAN em suas colunas.
-#pronto para utilizar em um proxima etapa.
+#pronto para utilizar em uma proxima etapa.
+print("Dimensão do início: ", volunteer.shape)
+
+print(volunteer.isnull().sum())
+
+volunteer_coluna = volunteer.drop(['amsl', 'amsl_unit', 'is_priority', 'primary_loc'], axis=1)
+print(volunteer_coluna.isnull().sum())
+
+volunteer_line = volunteer_coluna.dropna()
+print(volunteer_line.isnull().sum())
+print(f'A dimensão final fica:', volunteer_line.shape)
