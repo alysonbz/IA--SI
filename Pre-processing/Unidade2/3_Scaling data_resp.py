6e2a9821ed00f83a93f7da566cd5645ef0712d5e
Ruan Lima <ruanlimaspt@gmail.com>
2024-11-12 15:12:27 -0300

    atividades

--- Pre-processing/Unidade2/3_Scaling data_resp.py
+++ Pre-processing/Unidade2/3_Scaling data_resp.py
@@ -11,15 +11,15 @@ wine = load_wine_dataset()
 # Create the scaler
 scaler = StandardScaler()
 
-X = wine.drop(['Quality'],axis=1)
+X = wine.drop(['Quality'], axis=1)
 
-#X_norm = scaler.fit_transform(X)
+# X_norm = scaler.fit_transform(X)
 X_norm = np.log(X)
 
 y = wine['Quality'].values
 
-print('variancia',X.var())
-print('variancia do dataset normalizado',X_norm.var())
+print('variancia', X.var())
+print('variancia do dataset normalizado', X_norm.var())
 
 # Split the dataset and labels into training and test sets
 X_train, X_test, y_train, y_test = train_test_split(X_norm, y, stratify=y, random_state=42)
@@ -27,7 +27,7 @@ X_train, X_test, y_train, y_test = train_test_split(X_norm, y, stratify=y, rando
 knn = KNeighborsClassifier()
 
 # Fit the k-nearest neighbors model to the training data
-knn.fit(X_train,y_train)
+knn.fit(X_train, y_train)
 
 # Score the model on the test data
 print('score', knn.score(X_test, y_test))
