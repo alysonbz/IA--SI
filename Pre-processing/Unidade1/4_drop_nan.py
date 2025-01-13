9677f77a9f7b670affa1119064db3a66fca28f06
RafaellVieira <rafaelvieira0123@alu.ufc.br>
2025-01-12 23:50:49 -0300

    p

--- Pre-processing/Unidade1/4_drop_nan.py
+++ Pre-processing/Unidade1/4_drop_nan.py
@@ -4,3 +4,13 @@ volunteer = load_volunteer_dataset()
 
 #Elabore um código para motrar o shape e dados do dataset volunteer corrigido, sem NAN em suas colunas.
 #pronto para utilizar em um proxima etapa.
+
+print(volunteer.shape)
+print(volunteer.isnull().sum())
+volunteercorrigido =volunter.drop(['is_priority', 'amsl', 'asml_unit', 'primary_loc'],axis =1)
+print(volunteercorrigido)
+
+volunteerfinal = volunteercorrigido.dropna()
+print(volunteerfinal)
+
+print(volunteerfinal.isnull().sum())
