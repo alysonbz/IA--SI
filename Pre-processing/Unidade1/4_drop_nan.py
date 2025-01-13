8ccddb8073a6e5cdea463d432f3163ec1766c783
Eryka-Rodrigues <eryka.rodrigues@alu.ufc.br>
2025-01-13 17:35:34 -0300

    atv class

--- Pre-processing/Unidade1/4_drop_nan.py
+++ Pre-processing/Unidade1/4_drop_nan.py
@@ -1,15 +1,19 @@
-from numpy.testing.print_coercion_tables import print_new_cast_table
-
 from src.utils import load_volunteer_dataset
 
 volunteer = load_volunteer_dataset()
 
 #Elabore um código para motrar o shape e dados do dataset volunteer corrigido, sem NAN em suas colunas.
 #pronto para utilizar em um proxima etapa.
-print(volunteer.info())
-print(volunteer.isnull().sum())
-volunteer_ery = volunteer.drop(['primary_loc','amsl','amsl_unit','is_priority'],axis = 1)
-print(volunteer_ery)
+#Contagem de linhas do dataframe original
+print(volunteer.shape)
 
-volunteerfinal = volunteer_ery.dropna()
-print(volunteerfinal.isnull().sum())
+#Checkagem da linhas nulas por coluna
+print(volunteer.isnull().sum())
+
+#Deletar colonas com quantidades absurdas de linhas nulas
+volunteer_drop = volunteer.drop(['is_priority', 'amsl', 'amsl_unit', 'primary_loc'], axis=1)
+
+#Limpando o resto do dataframe
+volunteer_clean = volunteer_drop.dropna()
+print(volunteer_clean.shape)
+print(volunteer_clean.isnull().sum())
