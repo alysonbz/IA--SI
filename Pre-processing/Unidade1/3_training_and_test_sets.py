89548e6335f656c958e5014bfd8cd4c491aaabbc
RafaellVieira <rafaelvieira0123@alu.ufc.br>
2025-01-13 14:11:54 -0300

    p

--- Pre-processing/Unidade1/3_training_and_test_sets.py
+++ Pre-processing/Unidade1/3_training_and_test_sets.py
@@ -1,13 +1,14 @@
 from src.utils import load_volunteer_dataset
 from sklearn.model_selection import  train_test_split
+from unicodedata import category
 
 volunteer = load_volunteer_dataset()
 
 # Exclua as colunas Latitude e Longitude de volunteer
-volunteer_new = ___
+volunteer_new = volunteer(['Latitude', 'Longitude'].drop)
 
 # Exclua as linhas com valores null da coluna category_desc de volunteer_new
-volunteer = __
+volunteer = volunteer_new.dropna(['category_desc'])
 
 # mostre o balanceamento das classes em 'category_desc'
 print(__,'\n','\n')
@@ -18,7 +19,7 @@ X = __
 # Crie um dataframe de labels com a coluna category_desc
 y =__
 
-# # Utiliza a a amostragem stratificada para separar o dataset em treino e teste
+# # Utiliza a amostragem stratificada para separar o dataset em treino e teste
 X_train, X_test, y_train, y_test = ___(__, __, stratify=__, random_state=42)
 
 # mostre o balanceamento das classes em 'category_desc' novamente
