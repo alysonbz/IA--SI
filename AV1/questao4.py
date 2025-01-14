d0d7eff9d27c6cde7e483720fb0319fb7be7c135
RafaellVieira <rafaelvieira0123@alu.ufc.br>
2025-01-14 02:39:46 -0300

    AV1

---
+++ AV1/questao4.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+from scipy.spatial.distance import cdist
+from collections import Counter
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt
+
+
