8fe4f628707349d63dae808dc6342fafb3f19a8c
wi2lgit <willgit93@gmail.ci=om>
2025-01-19 17:41:33 -0300

    av1

--- Supervised_Learning/unidade4/4_fit_prediction.py
+++ Supervised_Learning/unidade4/4_fit_prediction.py
@@ -1,13 +1,12 @@
 from src.utils import load_sales_clean_dataset
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+
 sales_df = load_sales_clean_dataset()
 
-# Import mean_squared_error
-from sklearn import mean_squared_error
-
 # Create X and y arrays
-X = sales_df.drop(["sales","influencer" ],axis=1)
+X = sales_df.drop(["sales","influencer"],axis=1)
 y = sales_df["sales"].values
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
@@ -16,17 +15,17 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_
 reg = LinearRegression()
 
 # Fit the model to the data
-reg.fit(X_train,y_train)
+reg.fit(X_train, y_train)
 
 # Make predictions
 y_pred = reg.predict(X_test)
 print("Predictions: {}, Actual Values: {}".format(y_pred[:2], y_test[:2]))
 
 # Compute R-squared
-r_squared = reg.score(X_test, y_test)
+r_squared = reg.score(X_test,y_test)
 
 # Compute RMSE
-rmse = mean_squared_error(X_test, y_test, squared=False)
+rmse = mean_squared_error(y_pred, y_test, squared=False)
 
 # Print the metrics
 print("R^2: {}".format(r_squared))
