ef70e65a40b9503363c3c95b7136e1299a8d5fd5
Ruan Lima <ruanlimaspt@gmail.com>
2025-01-21 19:47:15 -0300

    av1

--- Supervised_Learning/unidade5/1_confusion_matrix.py
+++ Supervised_Learning/unidade5/1_confusion_matrix.py
@@ -4,18 +4,18 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report, confusion_matrix
 
 diabetes_df = load_diabetes_clean_dataset()
-X = diabetes_df.drop(['diabetes'],axis=1)
-y = diabetes_df['diabetes'].values
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42,stratify=y)
 
-knn = KNeighborsClassifier(n_neighbors=6)
+print(diabetes_df.isnull().sum())
 
-# Fit the model to the training data
-knn.fit(X_train, y_train)
+#X = diabetes_df.drop(['diabetes'],axis=1)
+#y = diabetes_df['diabetes'].values
+#X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42,stratify=y)
 
-# Predict the labels of the test data: y_pred
-y_pred = knn.predict(X_test)
+#knn = KNeighborsClassifier(n_neighbors=6)
 
-# Generate the confusion matrix and classification report
-print(classification_report(y_test, y_pred))
-print(confusion_matrix(y_test, y_pred))
+#knn.fit(X_train, y_train)
+
+#y_pred = knn.predict(X_test)
+
+#print(classification_report(y_test, y_pred))
+#print(confusion_matrix(y_test, y_pred))
