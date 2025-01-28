42b8ad1953f60ebed75d7a7b4eb293dcab90d960
mizaelcgomes <mizaelcgomespessoal@gmail.com>
2025-01-28 14:08:33 -0300

    Atividade de Classe 7

--- Supervised_Learning/unidade5/1_confusion_matrix.py
+++ Supervised_Learning/unidade5/1_confusion_matrix.py
@@ -4,9 +4,13 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report, confusion_matrix
 
 diabetes_df = load_diabetes_clean_dataset()
+
+
 X = diabetes_df.drop(['diabetes'],axis=1)
 y = diabetes_df['diabetes'].values
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42,stratify=y)
+
+
 
 knn = KNeighborsClassifier(n_neighbors=6)
 
--- Supervised_Learning/unidade5/3_logistic_regression.py
+++ Supervised_Learning/unidade5/3_logistic_regression.py
@@ -1,22 +1,25 @@
 from src.utils import load_diabetes_clean_dataset
 from sklearn.model_selection import train_test_split
-#Import LogisticRegression
-____
+from sklearn.linear_model import LogisticRegression  # Corrigido aqui
 
-
+# Carregar o conjunto de dados
 diabetes_df = load_diabetes_clean_dataset()
-X = diabetes_df.drop(['diabetes'],axis=1)
+
+# Separar as características e o alvo
+X = diabetes_df.drop(['diabetes'], axis=1)
 y = diabetes_df['diabetes'].values
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42,stratify=y)
 
+# Dividir o conjunto de dados em conjuntos de treinamento e teste
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42, stratify=y)
 
-# Instantiate the model
-logreg = ____
+# Instanciar o modelo
+logreg = LogisticRegression()
 
-# Fit the model
-____
+# Ajustar o modelo
+logreg.fit(X_train, y_train)
 
-# Predict probabilities
-y_pred_probs = logreg.____(____)[____, ____]
+# Prever as probabilidades
+y_pred_probs = logreg.predict_proba(X_test)[:, 1]
 
+# Exibir as primeiras 10 previsões de probabilidade
 print(y_pred_probs[:10])
