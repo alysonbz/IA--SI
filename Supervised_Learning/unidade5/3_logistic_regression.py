9ca552a9d62570f50c68f61a16b76b66e7dd0995
ruanfrota <ruan.frota13@alu.ufc.br>
2025-01-31 14:54:53 -0300

    unidade5 que eu fui nas aulas

--- Supervised_Learning/unidade5/3_logistic_regression.py
+++ Supervised_Learning/unidade5/3_logistic_regression.py
@@ -1,22 +1,24 @@
 from src.utils import load_diabetes_clean_dataset
 from sklearn.model_selection import train_test_split
-#Import LogisticRegression
-____
-
+from sklearn.linear_model import LogisticRegression  # Correct import
 
 diabetes_df = load_diabetes_clean_dataset()
-X = diabetes_df.drop(['diabetes'],axis=1)
+
+# Features and target variable
+X = diabetes_df.drop(['diabetes'], axis=1)
 y = diabetes_df['diabetes'].values
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42,stratify=y)
 
+# Split the data into training and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42, stratify=y)
 
-# Instantiate the model
-logreg = ____
+# Instantiate the Logistic Regression model
+logreg = LogisticRegression()
 
 # Fit the model
-____
+logreg.fit(X_train, y_train)
 
-# Predict probabilities
-y_pred_probs = logreg.____(____)[____, ____]
+# Predict probabilities for the test set
+y_pred_probs = logreg.predict_proba(X_test)  # Provide X_test here
 
+# Print the probabilities for the first 10 samples
 print(y_pred_probs[:10])
