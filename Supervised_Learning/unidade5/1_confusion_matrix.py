ea744c42e8ab9ee395208e02d0b0967f51c31e0a
PauloJr000 <paulojr00@alu.ufc.br>
2025-02-04 11:08:57 -0300

    oi

--- Supervised_Learning/unidade5/1_confusion_matrix.py
+++ Supervised_Learning/unidade5/1_confusion_matrix.py
@@ -1,9 +1,10 @@
+'''
 from src.utils import load_diabetes_clean_dataset
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 
 #Import confusion matrix
-____
+from sklearn.metrics import classification_report, confusion_matrix
 
 diabetes_df = load_diabetes_clean_dataset()
 X = diabetes_df.drop(['diabetes'],axis=1)
@@ -13,11 +14,62 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_
 knn = KNeighborsClassifier(n_neighbors=6)
 
 # Fit the model to the training data
-____
+knn.fit(X_train, y_train)
 
 # Predict the labels of the test data: y_pred
-y_pred = ____
+y_pred = knn.predict(X_test)
 
 # Generate the confusion matrix and classification report
-print(____(____, ____))
-print(____(____, ____))
+print(confusion_matrix(y_test, y_pred))
+print(classification_report(y_test, y_pred))
+
+'''
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.neighbors import KNeighborsClassifier
+from imblearn.over_sampling import SMOTE
+from src.utils import load_diabetes_clean_dataset
+
+# Carregar o conjunto de dados
+diabetes_df = load_diabetes_clean_dataset()
+X = diabetes_df.drop(['diabetes'], axis=1)
+y = diabetes_df['diabetes'].values
+
+# Aplicar SMOTE para balancear as classes
+smote = SMOTE(random_state=42)
+X_resampled, y_resampled = smote.fit_resample(X, y)
+
+# Dividir os dados em treinamento e teste
+X_train, X_test, y_train, y_test = train_test_split(
+    X_resampled, y_resampled, test_size=0.3, random_state=42, stratify=y_resampled
+)
+
+# Normalizar os dados
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+# Definir o modelo KNN e a grade de parâmetros
+knn = KNeighborsClassifier()
+param_grid = {
+    'n_neighbors': [3, 5, 7, 9, 11],
+    'weights': ['uniform', 'distance'],
+    'metric': ['euclidean', 'manhattan']
+}
+
+# Otimizar hiperparâmetros usando GridSearchCV
+grid_search = GridSearchCV(knn, param_grid, cv=5, scoring='f1_weighted')
+grid_search.fit(X_train, y_train)
+
+# Melhor modelo
+best_knn = grid_search.best_estimator_
+
+# Fazer previsões
+y_pred = best_knn.predict(X_test)
+
+# Imprimir a matriz de confusão e o relatório de classificação
+print("Melhores hiperparâmetros:", grid_search.best_params_)
+print(confusion_matrix(y_test, y_pred))
+print(classification_report(y_test, y_pred))
+
