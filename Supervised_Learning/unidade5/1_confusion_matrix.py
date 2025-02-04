586bc8c5e7b44e66c5743fc9a5a39ee561d39cc2
Ruan Lima <ruanlimaspt@gmail.com>
2025-02-04 10:41:31 -0300

    atv

--- Supervised_Learning/unidade5/1_confusion_matrix.py
+++ Supervised_Learning/unidade5/1_confusion_matrix.py
@@ -7,15 +7,15 @@ diabetes_df = load_diabetes_clean_dataset()
 
 print(diabetes_df.isnull().sum())
 
-#X = diabetes_df.drop(['diabetes'],axis=1)
-#y = diabetes_df['diabetes'].values
-#X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42,stratify=y)
+X = diabetes_df.drop(['diabetes'],axis=1)
+y = diabetes_df['diabetes'].values
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42,stratify=y)
 
-#knn = KNeighborsClassifier(n_neighbors=6)
+knn = KNeighborsClassifier(n_neighbors=6)
 
-#knn.fit(X_train, y_train)
+knn.fit(X_train, y_train)
 
-#y_pred = knn.predict(X_test)
+y_pred = knn.predict(X_test)
 
-#print(classification_report(y_test, y_pred))
-#print(confusion_matrix(y_test, y_pred))
+print(classification_report(y_test, y_pred))
+print(confusion_matrix(y_test, y_pred))
