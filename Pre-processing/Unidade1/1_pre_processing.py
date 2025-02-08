f96b7d395e7e2bcdfe92652b9813b5aa762c374d
Basílio <basiliobrandao@alu.ufc.br>
2025-02-08 13:09:02 -0300

    atividades

--- Pre-processing/Unidade1/1_pre_processing.py
+++ Pre-processing/Unidade1/1_pre_processing.py
@@ -3,7 +3,6 @@ from src.utils import load_volunteer_dataset
 volunteer = load_volunteer_dataset()
 
 # Mostre a dimensão do dataset volunteer
-<<<<<<< HEAD
 print(volunteer.shape)
 
 #mostre os tipos de dados existentes no dataset
@@ -11,15 +10,6 @@ print(volunteer.info())
 
 #mostre quantos elementos do dataset estão faltando na coluna
 print(volunteer['locality'].isnull().sum())
-=======
-
-
-#mostre os tipos de dados existentes no dataset
-
-
-#mostre quantos elementos do dataset estão faltando na coluna
-
->>>>>>> 8dbee5f0bdad0e083bc03654e1a4101bf868fd0d
 
 # Exclua as colunas Latitude e Longitude de volunteer
 volunteer_cols = volunteer.drop(['Latitude', 'Longitude'], axis= 1)
@@ -28,10 +18,6 @@ volunteer_cols = volunteer.drop(['Latitude', 'Longitude'], axis= 1)
 volunteer_subset = volunteer_cols.dropna(subset = 'category_desc')
 
 # Print o shape do subset
-<<<<<<< HEAD
 print(volunteer.subset.shape)
-=======
-print(volunteer_subset.shape)
->>>>>>> 8dbee5f0bdad0e083bc03654e1a4101bf868fd0d
 
 
--- Pre-processing/Unidade1/4_drop_nan.py
+++ Pre-processing/Unidade1/4_drop_nan.py
@@ -2,23 +2,14 @@ from src.utils import load_volunteer_dataset
 
 volunteer = load_volunteer_dataset()
 
-<<<<<<< HEAD
 #Elabore um código para motrar o shape e dados do dataset volunteer corrigido, sem NAN em suas colunas.
 #pronto para utilizar em um proxima etapa.
-#Contagem de linhas do dataframe original
 print(volunteer.shape)
-
-#Checkagem da linhas nulas por coluna
 print(volunteer.isnull().sum())
+print (volunteercorrigido = volunteer.drop(['is_priority', 'amsl_unit', 'primary_loc' ]), axis = 1)
+print(volunteercorrigido)
 
-#Deletar colonas com quantidades absurdas de linhas nulas
-volunteer_drop = volunteer.drop(['is_priority', 'amsl', 'amsl_unit', 'primary_loc'], axis=1)
+volunteerfinal = volunteercorrigido.dropna()
+print(volunteerfinal)
 
-#Limpando o resto do dataframe
-volunteer_clean = volunteer_drop.dropna()
-print(volunteer_clean.shape)
-print(volunteer_clean.isnull().sum())
-=======
-## realize print do dataset volunteer corrigido sem nenhum NAN, para isto removam as colunas NAN e depois as linhas e crie
-#um dataframe novo e print este mostrando a contagem de colunas NAN existentes e mostre também o shape novo.
->>>>>>> 8dbee5f0bdad0e083bc03654e1a4101bf868fd0d
+print(volunteerfinal.isnull().sum())
