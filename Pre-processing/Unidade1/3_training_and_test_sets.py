614945385255d31d1a1e618a88a30c4b5b4ea59a
LAÍSA <laiisaa.gomes@gmail.com>
2026-09-11 13:21:01 -0300

    AV2

--- Pre-processing/Unidade1/3_training_and_test_sets.py
+++ Pre-processing/Unidade1/3_training_and_test_sets.py
@@ -22,7 +22,7 @@ print(X)
 y = volunteer[['category_desc']]
 
 # Utiliza a a amostragem stratificada para separar o dataset em treino e teste
-X_train, X_test, y_train, y_test = train_test_split(X, y, stratify= y, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, stratify= y,test_size=0.2, random_state=42)
 
 # mostre o balanceamento das classes em 'category_desc' novamente
 print(y['category_desc'].value_counts())
