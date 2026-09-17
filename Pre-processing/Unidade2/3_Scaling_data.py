db2f9333bac1a1f4f33a15fc2d15142c2f10a4d7
myasmim-dez <yasmimcoelho@alu.ufc.br>
2026-09-17 09:31:56 -0300

    modificado até ' ATE AQUI FUNCIONA..'

--- Pre-processing/Unidade2/3_Scaling_data.py
+++ Pre-processing/Unidade2/3_Scaling_data.py
@@ -1,10 +1,11 @@
 # Import StandardScaler
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
-from src.utils import load_wine_dataset
 from sklearn.neighbors import KNeighborsClassifier
 import pandas as pd
+from src.utils import load_wine_dataset
 
+#df_scaled = pd.DataFrame(scaler.fit_transform(df),columns = df.columns)
 
 wine = load_wine_dataset()
 
@@ -18,16 +19,19 @@ X = wine.drop(['Quality'],axis=1)
 X_norm = scaler.fit_transform(X)
 
 #obtenha as labels da coluna Quality
-y = wine[''].__
+y = wine['Quality'].var
 
 #print a valriância de X
-print('variancia',__)
+print('variancia',X)
 
 #print a variânca do dataset X_norm
-print('variancia do dataset normalizado',__)
+print('variancia do dataset normalizado',X_norm)
+
+
+#ATE AQUI FUNCIONA ... ABAIXO AINDA NAO!
 
 # Divida o dataset em treino e teste com amostragem estratificada
-X_train, X_test, y_train, y_test = ___(___, __, ___, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X_norm, ___, ___, random_state=42)
 
 #inicialize o algoritmo KNN
 knn = ___
