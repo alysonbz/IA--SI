8da0277faee876a61149a74655f351d7bf600d96
yasmim_coelho <yasmimcoelho@alu.ufc.br>
2026-09-19 11:37:45 -0300

    (T_T)

--- Pre-processing/Unidade1/4_drop_nan.py
+++ Pre-processing/Unidade1/4_drop_nan.py
@@ -3,10 +3,12 @@ from src.utils import load_volunteer_dataset
 volunteer = load_volunteer_dataset()
 
 #a partir daqui é meu. e acho q só tira as colunas
-print("dataset:\n",volunteer)
+print("*--> dataset original:<--*\n",volunteer)
 volunteer_novo = volunteer.dropna(axis=1)
 
 print("\n *--> dataframe sem 'NaN':\n",volunteer_novo)
+
+print("\n *-->tamanho do novo dataframe<-- \n", volunteer_novo.shape)
 
 ## realize print do dataset volunteer corrigido sem nenhum NAN, para isto removam as colunas NAN e depois as linhas e crie
 #um dataframe novo e print este mostrando a contagem de colunas NAN existentes e mostre também o shape novo.
