42a983b96f7d9cec591e59c6d1de7b0425310756
LeviNascimento <levi63232@gmail.com>
2026-09-24 09:17:53 -0300

    Unidades 1 e 2

--- Supervised_Learning/unidade3/0_class_anotation.py
+++ Supervised_Learning/unidade3/0_class_anotation.py
@@ -1,7 +1,10 @@
+from pandas.core.algorithms import value_counts_internal
+
 from src.utils import load_churn_dataset
 from sklearn.neighbors import KNeighborsClassifier
 import numpy as np
 
 churn_df = load_churn_dataset()
+print(churn_df['area_code'].value_counts())
 
 
